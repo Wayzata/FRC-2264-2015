9679b1f2e03eb1023442bd66588c423b86708016
Nathan Ringo <tikiking1@gmail.com>
2015-01-09 18:29:40 -0600

    Adds lift, claw motor IDs.

--- src/org/usfirst/frc2264/RobotParts.java
+++ src/org/usfirst/frc2264/RobotParts.java
@@ -3,9 +3,13 @@ package org.usfirst.frc2264;
 /**
  * RobotParts is a class of constants that represent the parts on the robot. 
  */
-public class RobotParts {
-	public static final int LEFT_FRONT_MOTOR = 1;
-	public static final int RIGHT_FRONT_MOTOR = 2;
-	public static final int LEFT_BACK_MOTOR = 3;
-	public static final int RIGHT_BACK_MOTOR = 4;
+public final class RobotParts {
+	public static final class DRIVE_MOTORS {
+		public static final int LEFT_FRONT = 1;
+		public static final int RIGHT_FRONT = 2;
+		public static final int LEFT_BACK = 3;
+		public static final int RIGHT_BACK = 4;
+	};
+	public static final int LIFT_MOTOR = 5;
+	public static final int CLAW_MOTOR = 6;
 }
