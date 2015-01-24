1bb5472a98466c3e7bf28ab0b8ce5bb836c23c61
Brandon Lu <lubrandon00@gmail.com>
2015-01-24 10:55:31 -0600

    Slows down the lift motor

--- org/usfirst/frc2264/teleoperated/TeleoperatedCommand.java
+++ org/usfirst/frc2264/teleoperated/TeleoperatedCommand.java
@@ -23,7 +23,7 @@ public class TeleoperatedCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Subsystems.lift.setSpeed(Subsystems.joystick.getZ());
+    	Subsystems.lift.setSpeed(-.2 * Subsystems.joystick.getZ());
     }
 
     // Make this return true when this Command no longer needs to run execute()
