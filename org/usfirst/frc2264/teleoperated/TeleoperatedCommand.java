4caf2665915e02168d5b187d67c688e8e87bf929
Nathan Ringo <tikiking1@gmail.com>
2015-01-28 11:03:29 -0600

    Fixes lingering compile errors.

--- org/usfirst/frc2264/teleoperated/TeleoperatedCommand.java
+++ org/usfirst/frc2264/teleoperated/TeleoperatedCommand.java
@@ -1,27 +1,16 @@
 package org.usfirst.frc2264.teleoperated;
 
-import org.usfirst.frc2264.subsystems.DriveSubsystemCAN;
-import org.usfirst.frc2264.subsystems.JoystickSubsystem;
-import org.usfirst.frc2264.subsystems.LiftSubsystem;
 import org.usfirst.frc2264.subsystems.Subsystems;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 
 public class TeleoperatedCommand extends Command {
-	public LiftSubsystem localLift; // Nix these once we fix these
-	public JoystickSubsystem localJoystick;
-	public DriveSubsystemCAN localDrive;
     public TeleoperatedCommand() {
         // TODO Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	localLift = new LiftSubsystem(0);
-    	localJoystick = new JoystickSubsystem();
-    	localDrive = new DriveSubsystemCAN();
-    	this.requires(localLift);
-    	this.requires(localJoystick);
-    	this.requires(localDrive);
-    	
+    	this.requires(Subsystems.drive);
+    	this.requires(Subsystems.joystick);
     }
 
     // Called just before this Command runs the first time
@@ -31,13 +20,13 @@ public class TeleoperatedCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	localLift.setSpeed(0.4 * localJoystick.getZ());
-    	localDrive.move(localJoystick.getX(), localJoystick.getY(), localJoystick.getThrottle());
+    	Subsystems.drive.move(Subsystems.joystick.getX(), Subsystems.joystick.getY(),
+    			Subsystems.joystick.getThrottle());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return localJoystick.isButtonPressed();
+    	return Subsystems.joystick.isButtonPressed();
     }
 
     // Called once after isFinished returns true
