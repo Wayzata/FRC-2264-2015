5afd5c74188ccfca96e137926995a5d5c14bdff5
Nathan Ringo <tikiking1@gmail.com>
2015-02-03 16:42:52 -0600

    Fixes Vision?

--- org/usfirst/frc2264/autonomous/Vision.java
+++ org/usfirst/frc2264/autonomous/Vision.java
@@ -1,14 +1,16 @@
-package org.usfirst.frc.team2264.robot;
+package org.usfirst.frc2264.autonomous;
+
+import org.usfirst.frc2264.Robot_2264;
+import org.usfirst.frc2264.subsystems.DriveSubsystem;
+
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team2264.robot.DriveSubsystem;
-import edu.wpi.first.wpilibj.Timer;
 
 public class Vision extends SampleRobot
 {
 	public void robotInit()
 	{ 
-		Robot vision = new Robot();
+		Robot_2264 vision = new Robot_2264();
 		vision.robotInit();
 		double distance = 0;
 		DriveSubsystem drive = new DriveSubsystem(1, 2, 3, 4);
@@ -16,11 +18,10 @@ public class Vision extends SampleRobot
 		do{
 			while (isAutonomous() && isEnabled())
 			{
-				vision.autonomous();
+				vision.autonomousPeriodic();
 				distance = SmartDashboard.getNumber("Distance");
 				if(SmartDashboard.getBoolean("IsTote"))
 				{
-					
 					drive.move(1, 1, 0);
 				}
 			}
