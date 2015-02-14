e4863c992588f65d69bbf070e648873ce4b4e5c4
Nathan Ringo <tikiking1@gmail.com>
2015-02-14 08:55:59 -0600

    Consolidates Debug into Util.

--- org/usfirst/frc2264/Debug.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc2264;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-public class Debug {
-	public static void log(String s) {
-		SmartDashboard.putString("Debug stuff", s);
-	}
-}
--- org/usfirst/frc2264/misc/Util.java
+++ org/usfirst/frc2264/misc/Util.java
@@ -3,8 +3,12 @@ package org.usfirst.frc2264.misc;
 import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Util {
+	public static void log(String s) {
+		SmartDashboard.putString("Debug stuff", s);
+	}
 	public static void waitFor(BooleanSupplier condition) {
 		while(!condition.getAsBoolean()) Timer.delay(0.01);
 	}
