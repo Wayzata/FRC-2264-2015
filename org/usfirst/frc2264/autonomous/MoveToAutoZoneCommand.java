1d64556b0fa083a2970204307120293e77b4b99d
Nathan Ringo <tikiking1@gmail.com>
2015-02-27 08:24:59 -0600

    Rewrites autonomous to hopefully work.

--- org/usfirst/frc2264/autonomous/MoveToAutoZoneCommand.java
+++ org/usfirst/frc2264/autonomous/MoveToAutoZoneCommand.java
@@ -6,19 +6,19 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class MoveToAutoZoneCommand extends Command {
-	private boolean die = false;
-	public MoveToAutoZoneCommand() {
+	private double wait, end;
+	public MoveToAutoZoneCommand() { this(2.5); }
+	public MoveToAutoZoneCommand(double wait) {
 		super("Autonomous > Move to Auto Zone");
+		this.wait = wait;
 		this.requires(Subsystems.drive);
 	}
-	protected void initialize() {}
-	protected void execute() {
+	protected void initialize() {
+		this.end = Timer.getFPGATimestamp() + this.wait;
 		Subsystems.drive.move(0.0, 1.0);
-		Timer.delay(2.5);
-		Subsystems.drive.stop();
-		this.die = true;
 	}
-	protected boolean isFinished() { return this.die; }
+	protected void execute() {}
+	protected boolean isFinished() { return Timer.getFPGATimestamp() >= this.end; }
 	protected void end() { Subsystems.drive.stop(); }
 	protected void interrupted() {}
 }
