5fb1ba9e8a4d14c8bf798c9d8c7a5b9a8bf2bcbb
Nathan Ringo <tikiking1@gmail.com>
2015-02-28 08:57:21 -0600

    Adds new autonomous functionality.

--- org/usfirst/frc2264/autonomous/AutonomousCommandGroup.java
+++ org/usfirst/frc2264/autonomous/AutonomousCommandGroup.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCommandGroup extends CommandGroup {
 	public AutonomousCommandGroup() {
 		this.addSequential(new GrabBoxCommand());
+		this.addSequential(new LiftBoxCommand());
 		this.addSequential(new MoveToAutoZoneCommand());
 	}
 }
--- org/usfirst/frc2264/autonomous/LiftBoxCommand.java
+++ org/usfirst/frc2264/autonomous/LiftBoxCommand.java
@@ -4,19 +4,18 @@ import org.usfirst.frc2264.subsystems.Subsystems;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class GrabBoxCommand extends Command {
-	public GrabBoxCommand() { this(1.5); }
-	public GrabBoxCommand(double timeout) {
-		this.requires(Subsystems.claw);
+public class LiftBoxCommand extends Command {
+	public LiftBoxCommand() { this(1.5); }
+	public LiftBoxCommand(double timeout) {
+		this.requires(Subsystems.lift);
 		this.setTimeout(timeout);
 	}
 	protected void initialize() {
 	}
 	protected void execute() {
-		Subsystems.claw.startClosing();
+		Subsystems.lift.set(1.0);
 	}
 	protected boolean isFinished() { return this.isTimedOut(); }
-	protected void end() { Subsystems.claw.stop(); }
+	protected void end() { Subsystems.lift.stop(); }
 	protected void interrupted() { this.end(); }
 }
-
